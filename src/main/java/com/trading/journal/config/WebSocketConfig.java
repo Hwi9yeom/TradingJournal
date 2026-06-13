9da5f7afe08167ca5a05f59d666eb4a98c2d1985
HwiGyeom <hwi9yeom@gmail.com>
2026-06-13 14:04:34 +0900

    feat: authenticate WebSocket handshake with JWT

--- src/main/java/com/trading/journal/config/WebSocketConfig.java
+++ src/main/java/com/trading/journal/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.trading.journal.config;
 
 import com.trading.journal.websocket.AlertWebSocketHandler;
+import com.trading.journal.websocket.JwtHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -13,10 +14,12 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final AlertWebSocketHandler alertWebSocketHandler;
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(alertWebSocketHandler, "/ws/alerts")
+                .addInterceptors(jwtHandshakeInterceptor)
                 .setAllowedOriginPatterns("http://localhost:*", "https://localhost:*");
     }
 }
